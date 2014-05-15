e05ffd00636a4f4de928cbdf8f297394ffdc9eb7
Adley <adley.phu@gmail.com>
2014-05-15 15:05:37 +1000

    combine Vertex.c and Vertex.h with Game.c

--- Game.c
+++ Game.c
@@ -19,8 +19,23 @@
 #define Y 2
 #define Z 3
 
-#define FILE_NAME "regions.txt"
+#define REGIONS_TXT "regions.txt"
 #define NUM_VERTICES_IN_A_REGION 6
+
+#define MAX_COORDINATE 6
+#define NUM_VERTICES 54
+
+#define MAX_COORDINATE 6
+#define NUM_VERTICES 54
+
+#define FILE_NAME "vertices.txt"
+
+struct _vertex{
+    int ID;
+    int contents;
+};
+
+typedef struct _vertex *Vertex;
 
 typedef struct _coordinate {
     int x, y, z;
@@ -50,6 +65,15 @@ struct _game {
     university universities[NUM_UNIS];
     region regions[NUM_REGIONS];
 };
+
+Vertex*** makeVertexMap(void);
+
+int getID(Vertex vertex);
+int getContents(Vertex vertex);
+int changeContents(Vertex vertex, int newElement);
+void freeMap(Vertex*** map);
+
+int sizeOfVertex(void);
 
 int directionToIndex(char direction);
 coordinate coordinateFromPath(Game g, path requestPath);
@@ -99,7 +123,7 @@ Game newGame (int discipline[], int dice[]) {
     x = 0;
     i = 0;
     // load in the regions and their associated vertices
-    FILE *fin = fopen(FILE_NAME, "r");
+    FILE *fin = fopen(REGIONS_TXT, "r");
     while (x < NUM_REGIONS) {
         g->regions[x].ID = x;
         while (i < NUM_VERTICES_IN_A_REGION) {
@@ -568,4 +592,90 @@ int getExchangeRate (Game g, int player,
 
 int isLegalAction (Game g, action a) {
     return TRUE;
+}
+////
+
+Vertex*** makeVertexMap(void){
+
+    FILE *fin = fopen(FILE_NAME, "r");
+    Vertex ***first = malloc(sizeof(Vertex**) * MAX_COORDINATE);
+
+    // Creates blank map
+    int countA = 0;
+    while (countA < MAX_COORDINATE){
+
+        Vertex **second = malloc(sizeof(Vertex*) * MAX_COORDINATE);
+
+        int countB = 0;
+        while (countB < MAX_COORDINATE){
+
+            Vertex *third = malloc(sizeof(Vertex) * MAX_COORDINATE);
+
+            int countC = 0;
+            while (countC < MAX_COORDINATE){
+
+                Vertex vertex = malloc(sizeof(struct _vertex));
+                vertex->ID = -1;
+                vertex->contents = 0;
+                third[countC] = vertex;
+                countC++;
+            }
+            second[countB] = third;
+            countB++;
+        }
+        first[countA] = second;
+        countA++;
+    }
+
+    int x, y, z;
+    int count = 0;
+    while (count < NUM_VERTICES){
+        // loads valid vertices
+
+        fscanf(fin, "%d %d %d", &x, &y, &z);
+        first[x][y][z]->ID = count;
+        count++;
+    }
+    return first;
+}
+
+int getID(Vertex vertex){
+    return vertex->ID;
+}
+
+int getContents(Vertex vertex){
+    return vertex->contents;
+}
+
+int changeContents(Vertex vertex, int newElement){
+    return vertex->contents = newElement;
+}
+
+void freeMap(Vertex*** map){
+
+    int countA = 0;
+
+    while (countA < MAX_COORDINATE){
+
+        int countB = 0;
+        while (countB < MAX_COORDINATE){
+
+
+            int countC = 0;
+            while (countC < MAX_COORDINATE){
+                free(map[countA][countB][countC]);
+                countC++;
+            }
+            free(map[countA][countB]);
+            countB++;
+        }
+        free(map[countA]);
+        countA++;
+    }
+    free(map);
+    return;
+}
+
+int sizeOfVertex(void){
+    return sizeof(struct _vertex);
 }
