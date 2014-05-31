2f42903b2337a636aa964c5f00c8aaa230f6e687
andairoo <andrew.dai@hotmail.com>
2014-05-31 18:29:27 +1000

    Create mrPass.c

---
+++ mrPass.c
@@ -0,0 +1,23 @@
+/*
+ *  Mr Pass.  Brain the size of a planet!
+ *
+ *  Proundly Created by Richard Buckland
+ *  Share Freely Creative Commons SA-BY-NC 3.0. 
+ *
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+#include "Game.h"
+#include "mechanicalTurk.h"
+ 
+action decideAction (Game g) {
+ 
+   
+   action nextAction;
+   nextAction.actionCode = PASS;
+   
+   return nextAction;
+}
